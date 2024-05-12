55292c733b53def887574319aedc21f0d3a50cb4
faberdemo <faberdemo@gmail.com>
2024-05-12 19:21:00 -0300

    Updated train to include lidar python argument

--- train.sh
+++ train.sh
@@ -2,18 +2,24 @@
 
 agent="${1:-ddpg}"
 stage="${2:-1}"
-# load_models="${3:-False}"
+lidar="${3:-10}"
+# load_models="${4:-False}"
 
-echo "Agent: $agent - Stage: $stage - Load_models=$load_models"
-
+echo "Agent: $agent - Stage: $stage - Lidar=$lidar"
 sleep 5
 
 
 echo "...Training..."
-python3 train.py --agent $agent --stage $stage &&
+python train.py --agent $agent --stage $stage --lidar $lidar
+sleep 5
+
 echo "...Saving models to best_models..."
-python3 save_to_best.py --agent  $agent --stage $stage &&
+python save_to_best.py --agent  $agent --stage $stage --lidar $lidar
+sleep 5
+
 echo "...Testing..."
-python3 test.py --agent $agent --stage $stage &&
+python test.py --agent $agent --stage $stage --lidar $lidar
+sleep 5
+
 echo "...Plotting learning curve..."
-python3 learning_curve.py --agent $agent --stage $stage
+python learning_curve.py --agent $agent --stage $stage --lidar $lidar
