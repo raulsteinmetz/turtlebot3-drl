97a5e5631b21723f67a83dc4a71e50d504e646b6
raulsteinmetz <rsteinmetz@inf.ufsm.br>
2024-05-29 09:20:59 -0300

    stage 6 spawns first attempt

--- train.py
+++ train.py
@@ -135,7 +135,7 @@ if __name__ == '__main__':
         if args.agent in config_data:
             configs.update(config_data[args.agent])
             configs['train_episodes'] = config_data[args.agent].get('train_episodes', 5001)
-            configs['max_steps_per_episode'] = config_data[args.agent].get('max_steps_per_episode', 300)
+            configs['max_steps_per_episode'] = config_data[args.agent].get('max_steps_per_episode', 2)
         else:
             raise ValueError(f"No configuration found for agent: {args.agent}")
 
